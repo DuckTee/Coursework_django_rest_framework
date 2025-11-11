79c4fe03cf42ad5236a42a6df1fb6b7e2afa0296
Ksenia_Sheveleva <brantova00@gmail.com>
2025-11-11 18:36:04 +0300

    доработки

--- habits/urls.py
+++ habits/urls.py
@@ -10,7 +10,6 @@ router.register(r'public-habits', PublicHabitViewSet, basename='public-habit')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('habits.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
